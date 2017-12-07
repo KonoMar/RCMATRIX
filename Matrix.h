d89ebd2f964546948e562570b665ca763af5e712
maestro_m <maestro_m@github.com>
2017-12-07 17:39:59 +0100

    Initial commit

---
+++ Matrix.h
@@ -0,0 +1,15 @@
+#ifndef __MATRIX_H__
+#define __MATRIX_H__
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>	/* setw(int) */
+using namespace std;
+
+class IndexOutOfRange{};
+class WrongDim{};
+
+//****************************************************************
+
+
+#endif /* __MATRIX_H__ */
