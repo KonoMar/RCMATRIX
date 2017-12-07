7db39d09a91b1a32423e7ad6872eb10750ed58cd
Marcin Konopka <marcinkonopka123@wp.pl>
2017-12-07 17:59:22 +0100

    001
    class CMatrix

--- Matrix.h
+++ Matrix.h
@@ -9,6 +9,31 @@ using namespace std;
 class IndexOutOfRange{};
 class WrongDim{};
 
+class CMatrix
+{
+    struct rcmatrix;
+    rcmatrix *mat;
+    
+public:
+    class Proxy;
+    class Proxy2;
+    CMatrix(fstream& f);
+    CMatrix(int r, int c, double val, double val2 = 0);
+    CMatrix(const CMatrix& x);
+    ~CMatrix();
+    CMatrix& operator=(const CMatrix& x);
+    CMatrix operator+(const CMatrix& x) const;
+    CMatrix operator*(const CMatrix& x) const;
+    friend ostream& operator<<(ostream&, const CMatrix&);
+    Proxy operator[](int index);
+    // operator[](int index);
+    inline double read(int i, int k) const;
+    inline void write(int w, int k, double d);
+};
+
+
+
+
 //****************************************************************
 
 
